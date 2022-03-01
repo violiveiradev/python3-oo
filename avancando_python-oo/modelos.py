4dde632ab30511c879561fa783c5c21d68b0655b
violiveiradev <violiveira.dev@gmail.com>
2022-02-28 22:41:44 -0300

    Introduzido conceito super classe

--- avancando_python-oo/modelos.py
+++ avancando_python-oo/modelos.py
@@ -1,61 +1,50 @@
-from os import lseek
-from termios import CKILL
+class Programa:
+    def __init__(self, nome, ano) -> None:
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    
+    @property
+    def likes(self):
+        return self._likes
+    
+
+    def dar_likes(self):
+        self._likes += 1
+
+    
+    @property
+    def nome(self):
+        return self._nome
+    
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
 
 
-class Filme:
+class Filme(Programa):
     def __init__(self, nome, ano, duracao) -> None:
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-    
-
-    def dar_likes(self):
-        self.__likes += 1
 
     
-    @property
-    def nome(self):
-        return self.__nome
-    
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
-    
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporada) -> None:
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporade = temporada
-        self.__likes = 0
 
     
-    @property
-    def likes(self):
-        return self.__likes
-    
 
-    def dar_likes(self):
-        self.__likes += 1
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
 
-    
-    @property
-    def nome(self):
-        return self.__nome
-    
+atlanta.dar_likes()
+atlanta.dar_likes()
 
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
-    
-
-vingadores = Filme("vingadores - guerra inifita", 2018, 106)
-print(vingadores)
-
-atlanta = Serie("atlanta", 2018, 2)
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}')
+print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
+print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
