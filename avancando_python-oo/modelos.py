74523ae027dc6aef31e85d00e35c7dbba1ec2e9a
violiveiradev <violiveira.dev@gmail.com>
2022-03-23 20:47:13 -0300

    Inserido classe playlist

--- avancando_python-oo/modelos.py
+++ avancando_python-oo/modelos.py
@@ -23,28 +23,60 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
+    def __str__(self) -> str:
+        return f"Nome: {self._nome} - Likes: {self._likes}"
+
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao) -> None:
         super().__init__(nome, ano)
         self.duracao = duracao
 
+
+    def __str__(self) -> str:
+        return f"Nome: {self._nome} - Duração: {self.duracao} - Ano: {self.ano} - Likes: {self._likes}"
+
     
 class Serie(Programa):
     def __init__(self, nome, ano, temporada) -> None:
         super().__init__(nome, ano)
         self.temporade = temporada
-
     
+
+    def __str__(self) -> str:
+        return f"Nome: {self._nome} - Temporadas: {self.temporade} - Ano: {self.ano} - Likes: {self._likes}"
+
+class Playlist():
+    def __init__(self, nome, programas) -> None:
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self.programas)
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
+tmep = Filme('todo mundo em panico', 1999, 100)
+demolidor = Serie('demolidor', 2016, 2)
 
+
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+lista_programas = [atlanta, vingadores, demolidor, tmep]
+minha_playlist = Playlist('fim de semana', lista_programas)
+
+for programa in minha_playlist.listagem:
+    print(programa)
